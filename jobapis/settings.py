3d8c78dc125733509c0938d0af53267fdc5220bc
owolabi-develop <owolabidevelop84@gmail.com>
2023-08-20 18:53:55 -0700

    update auth

--- jobapis/settings.py
+++ jobapis/settings.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import environ
-env = environ.Env()
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
